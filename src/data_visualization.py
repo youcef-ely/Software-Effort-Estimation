965b1a3bbb3636342cc4c1c2d75ed661fe1730b2
youcef-ely <76822707+youcef-ely@users.noreply.github.com>
2022-04-09 09:04:30 +0000

    Visualize Categorical Data

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,35 +1,45 @@
+from cv2 import normalize
 import seaborn as sns
 import matplotlib.pyplot as plt 
 import pandas as pd
-from sympy import rotations 
 pd.set_option('display.max_row', 70)
 pd.set_option('display.max_column', 70)
 
 
-def pie_plot(column, values_meaning):
+def pie_plot(data_column, values_meaning):
+    if values_meaning != None:
+        data_column = data_column.replace(values_meaning)
     fig = plt.figure(figsize = (10, 10))
     fig.patch.set_facecolor('white')
     labels = None
-    if values_meaning != {}:
-        labels = values_meaning.values()
-    column.value_counts().sort_index().plot(kind = 'pie',  labels = labels, autopct = '%.2f')
+    data_column.value_counts().plot(kind = 'pie', autopct = '%.2f')
 
 
 def histplot(column):
     sns.distplot(column.value_counts().sort_index())
 
-def barplot(x, y, title):
-    plt.figure(figsize = (12, 12))
-    sns.barplot(x = x, y = y)
+
+def bar_plot_count(data_column, title, values_meaning = None):
+    if values_meaning != None:
+        data_column = data_column.replace(values_meaning)
+    plt.figure(figsize = (12, 9))
+    counts = data_column.value_counts(normalize = True, sort = False)
+    ax     = sns.barplot(x = counts.index, y = counts.values)
+    ax.bar_label(ax.containers[0], fmt = '%.2f')
     plt.title(title)
     plt.xticks(rotation = 75)
+
 
 
 def nan_columns_rates(data):
     return (data.isna().sum() / data.shape[0]).sort_values(ascending = False)
 
+
+
 def nan_rows_rates(data):
     return data.isna().sum(axis = 1).sort_values(ascending = False) / data.shape[1]
+
+
 
 def missing_values_map(data):
     sns.heatmap(data.isna())
